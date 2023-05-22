ab8048217912787e4ea19e15ba2001622b0c77cc
Gareth Jones <oxidising@gmail.com>
2023-05-22 15:10:23 +0100

    Improving RasterLayer name handling with better date parsing

--- R/shiny_mapper.R
+++ R/shiny_mapper.R
@@ -1,18 +1,27 @@
 #' Shiny app fo plotting raster data
 #'
-#' @param raster_data
-#' @param polygon_data
+#' @param raster_data Raster data in the form or a RasterStack or RasterBrick
+#' @param polygon_data Polygon data to plot on map
+#' @param date_format A date format that will be passed in to lubridate::as_date
 #'
 #' @return
 #' @keywords internal
 #'
 #' @examples
-raster_mapping_app <- function(raster_data = NULL, polygon_data = NULL) {
+raster_mapping_app <- function(raster_data = NULL, polygon_data = NULL, date_format = NULL) {
+    # Remove any letters from the names of the RasterLayers
+    cleaned_names <- stringr::str_replace_all(names(raster_data), pattern = "[A-Za-z]", replacement = "")
+    # Create Date objects
+    date_strings <- lubridate::as_date(cleaned_names, format = date_format)
+    date_strings <- base::sort(date_strings)
 
-    # Create a named list so we can display the dates
-    # NOTE - So RasterLayer names can't start with a number
-    date_strings <- lubridate::as_date(sub("X", "", sort(names(raster_data))))
-    date_list <- as.list(names(raster_data))
+    if (any(is.na(lubridate::parse_date_time(names(raster_data), orders = date_format)))) {
+        stop("Unable to parse dates from layer names. Please ensure they are named correctly or pass the date_format argument with the correct format.")
+    }
+
+    # We want to be able to lookup the RasterLayer names using the date so we create a list
+    date_list <- as.list(base::sort(names(raster_data)))
+    # Then we assign the date strings we'll use in the UI as the name (or key) for each element in the list
     names(date_list) <- date_strings
 
     # Define UI for application that draws a histogram
@@ -43,12 +52,19 @@ raster_mapping_app <- function(raster_data = NULL, polygon_data = NULL) {
             layer_name <- date_list[[as.character(input$date_slider)]]
             raster_image <- raster_data[[layer_name]]
 
-            leaflet::leaflet() %>%
-                leaflet::addTiles() %>%
-                leaflet::addRasterImage(
+            m <- leaflet::leaflet()
+            m <- leaflet::addTiles(m)
+
+            if (!is.null(raster_data)) {
+                m <- leaflet::addRasterImage(m,
                     x = raster_image,
                     opacity = 1
                 )
+            }
+
+            if (!is.null(polygon_data)) {
+                m <- leaflet::addPolygons(m, data = polygon_data, weight = 2)
+            }
         })
 
         output$selected_date <- shiny::renderText({
@@ -65,12 +81,13 @@ raster_mapping_app <- function(raster_data = NULL, polygon_data = NULL) {
 #' Run the interactive plotting Shiny app
 #'
 #' @param raster_data Raster data in the form or a RasterStack or RasterBrick
-#' @param polygon_data
+#' @param polygon_data Polygon data to plot on map
+#' @param date_format A date format that will be passed in to lubridate::as_date
 #'
 #' @return NULL
 #' @export
-plot_interative_map <- function(raster_data = NULL, polygon_data = NULL) {
+plot_interative_map <- function(raster_data = NULL, polygon_data = NULL, date_format = NULL) {
     require_packages(packages = c("leaflet", "shiny", "stringr"))
 
-    shiny::runApp(raster_mapping_app())
+    shiny::runApp(raster_mapping_app(raster_data = raster_data, polygon_data = polygon_data, date_format = date_format))
 }
