5aa6bee65e98a488c195560e1d91f444a2ec095c
Gareth Jones <oxidising@gmail.com>
2023-05-31 11:11:58 +0100

    WIP: More customisable plotting of polygon and markers

--- R/plot_mapping.R
+++ R/plot_mapping.R
@@ -1,42 +1,50 @@
 #' Create a simple Leaflet map from data
 #'
-#' @param data Polygon data
-#' @param domain Domain for map
-#' @param markers A vector of lists with lat/long and names for markers on the map
+#' @param polygon_data Polygon data
+#' @param colour_domain Colour domain for polygon on map
+#' @param markers A data.frame containing latitude, longitude and label columns
 #' @param palette Palette, for example YlOrRd or Reds
 #' @param legend_values Values for legend
 #' @param legend_title Title for legend
 #' @param add_scale_bar Add scale bar if TRUE
 #' @param polygon_fill_opacity Leaflet polygon fill opacity, float from 0 to 1.0, passed to fillOpacity of leaflet::addPolygons
+#' @param fill_colour Fill colour of polygon, overriden if colour_domain is passed
 #' @param fill_colour_weight Polygon colour weight, float from 0 to 1.0, Passed to the weight argument of addPolygons
 #'
 #' @return NULL
 #' @export
 #' @importFrom magrittr %>%
-plot_map <- function(data = NULL,
+plot_map <- function(polygon_data = NULL,
+                     colour_domain = NULL,
                      markers = NULL,
-                     domain = NULL,
                      palette = "YlOrRd",
-                     colour = "grey",
+                     line_colour = "grey",
                      legend_values = NULL,
                      legend_title = NULL,
                      add_scale_bar = FALSE,
-                     polygon_fill_opacity = 0.75,
+                     reverse_colours = FALSE,
+                     fill_opacity = 0.75,
+                     fill_colour = "#ba2d2d",
                      fill_colour_weight = 1.0) {
   require_packages(packages = "leaflet")
-
-  # colours <- leaflet::colorNumeric(palette = palette, domain = domain, reverse = FALSE)
 
   m <- leaflet::leaflet()
   m <- leaflet::addTiles(m)
 
-  m <- leaflet::addPolygons(m,
-    data = data,
-    # fillColor = ~colours(domain), color = colour, weight = fill_colour_weight,
-    # fillColor = ~colours(), color = colour, weight = fill_colour_weight,
-    fillColor = "red",
-    fillOpacity = polygon_fill_opacity
-  )
+  if (!is.null(colour_domain)) {
+    colours <- leaflet::colorNumeric(palette = palette, domain = colour_domain, reverse_colours = FALSE)
+    fill_colour <- ~ colours(colour_domain)
+  }
+
+  if (!is.null(polygon_data)) {
+    m <- leaflet::addPolygons(m,
+      data = polygon_data,
+      fillColor = fill_colour,
+      color = line_colour,
+      weight = fill_colour_weight,
+      fillOpacity = fill_opacity
+    )
+  }
 
   if (!is.null(markers)) {
     m <- leaflet::addMarkers(m, lng = markers$longitude, lat = markers$latitude, label = markers$label)
@@ -46,14 +54,14 @@ plot_map <- function(data = NULL,
     m <- leaflet::addScaleBar(m, position = "bottomleft")
   }
 
-  # if (!is.null(legend_values)) {
-  #   m <- leaflet::addLegend(m,
-  #     pal = colours,
-  #     values = legend_values,
-  #     opacity = 0.8,
-  #     title = legend_title
-  #   )
-  # }
+  if (!is.null(legend_values)) {
+    m <- leaflet::addLegend(m,
+      pal = colours,
+      values = legend_values,
+      opacity = 0.8,
+      title = legend_title
+    )
+  }
   return(m)
 }
 
