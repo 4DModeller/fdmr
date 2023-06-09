7c7d367635628eef5ea5d9ca1cbdae89884e77b6
Gareth Jones <oxidising@gmail.com>
2023-06-09 17:08:10 +0100

    Updated param default checks and vignette

--- R/shiny_meshbuilder.R
+++ R/shiny_meshbuilder.R
@@ -37,14 +37,14 @@ meshbuilder_shiny <- function(
     max_edge = NULL,
     offset = NULL,
     cutoff = NULL) {
-    # TODO - add in CRS reading from data
-    # TODO - these defaults need changing,
+    # TODO - these defaults need changing?
     max_edge_default <- c(0.1, 0.3)
     offset_default <- c(0.2, 0.7)
     cutoff_default <- 0.2
 
-    # Let's extract the data we want to create the mesh
-    location_data <- spatial_data@data[, c("LONG", "LAT")]
+    if (is.null(max_edge)) max_edge <- max_edge_default
+    if (is.null(offset)) offset <- offset_default
+    if (is.null(cutoff)) cutoff <- cutoff_default
 
     if (is.null(crs)) {
         crs <- sf::st_crs(spatial_data)
@@ -52,6 +52,9 @@ meshbuilder_shiny <- function(
             stop("Unable to read CRS from data, please pass proj4string CRS to crs argument.")
         }
     }
+
+    # Let's extract the data we want to create the mesh
+    location_data <- spatial_data@data[, c("LONG", "LAT")]
 
     ui <- shiny::fluidPage(
         shiny::sidebarLayout(
@@ -73,7 +76,7 @@ meshbuilder_shiny <- function(
                 )
             ),
             shiny::mainPanel(
-                leaflet::leafletOutput("map")
+                leaflet::leafletOutput("map", height = "80vh")
             )
         )
     )
@@ -95,53 +98,17 @@ meshbuilder_shiny <- function(
         })
 
         mesh_spatial <- shiny::reactive({
-            fdmr::mesh_to_spatial(mesh = mesh())
+            shiny::withProgress(message = "Extracting mesh...", value = 0, {
+                fdmr::mesh_to_spatial(mesh = mesh())
+            })
         })
 
         output$map <- leaflet::renderLeaflet({
             leaflet::leaflet(mesh_spatial()) %>%
                 leaflet::addTiles() %>%
-                leaflet::addPolygons(weight = 0.5, fillOpacity = 0.2, fillColor = "blue") %>%
-                leaflet::addPolygons(data = spatial_data, fillColor = "red", weight = 1)
-            # leaflet::addPolygons(data = spatial_data, lng = location_data$LONG, lat = location_data$LAT, fillColor = "red", color = "white", weight = 0.5)
-
-            # leaflet::leaflet(mesh_spatial()) %>%
-            #     leaflet::addTiles() %>%
-            #     leaflet::addPolygons(data = spatial_data, fillColor = "#d66363", stroke = 0.2)
+                leaflet::addPolygons(weight = 0.5, fillOpacity = 0.2, fillColor = "#5252ca") %>%
+                leaflet::addPolygons(data = spatial_data, fillColor = "#d66363", weight = 1)
         })
-
-        # # From here to ylim taken from INLA's meshbuilder
-        # mesh_proj <- shiny::reactive({
-        #     if (is.null(mesh())) {
-        #         NULL
-        #     } else {
-        #         INLA::inla.mesh.projector(mesh(), dims = c(500, 500))
-        #     }
-        # })
-
-        # xlim <- shiny::reactive({
-        #     lim1 <- if (is.null(mesh())) NA else range(mesh()$loc[, 1], na.rm = TRUE)
-
-        #     r <- c(lim1)
-        #     if (all(is.na(r))) {
-        #         r <- c(0, 1)
-        #     } else {
-        #         r <- range(r, na.rm = TRUE)
-        #     }
-        #     r
-        # })
-
-        # ylim <- shiny::reactive({
-        #     lim1 <- if (is.null(mesh())) NA else range(mesh()$loc[, 2], na.rm = TRUE)
-
-        #     r <- lim1
-        #     if (all(is.na(r))) {
-        #         r <- c(0, 1)
-        #     } else {
-        #         r <- range(r, na.rm = TRUE)
-        #     }
-        #     r
-        # })
 
 
 
@@ -191,11 +158,12 @@ meshbuilder_shiny <- function(
 #'
 #' @return shiny::app
 #' @export
-mesh_builder <- function(spatial_data, max_edge, offset, cutoff) {
+mesh_builder <- function(spatial_data, crs = NULL, max_edge = NULL, offset = NULL, cutoff = NULL) {
     require_packages(packages = c("INLA", "shiny", "leaflet"))
 
     shiny::runApp(meshbuilder_shiny(
         spatial_data = spatial_data,
+        crs = crs,
         max_edge = max_edge,
         offset = offset,
         cutoff = cutoff
