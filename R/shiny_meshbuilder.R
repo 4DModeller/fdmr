528dcd73a9b95eda2fdba6ddb58d5f43461f9b0a
Gareth Jones <oxidising@gmail.com>
2023-09-11 10:07:45 +0100

    Logic to plot polygons or circles depending on data

--- R/shiny_meshbuilder.R
+++ R/shiny_meshbuilder.R
@@ -35,51 +35,15 @@ meshbuilder_shiny <- function(
   if (is.null(offset)) offset <- default_offset
   if (is.null(cutoff)) cutoff <- default_cutoff
 
-  # plot_poly <- FALSE
-  # plot_points <- FALSE
-  # We
-  # spatial_points <- NULL
-  # Do some checks to see what kind of data we have
-  # If we have a SpatialPolygonsDataFrame, we can plot the polygons
-  # otherwise if we just have SpatialPoints we can plot the points
-  # otherwise we don't plot anything
-  # if (class(spatial_data) == "SpatialPolygonsDataFrame") {
-  #   plot_poly <- TRUE
-  #   plot_points <- FALSE
-  # } else {
-  #   # Try and create some points from the dataframe
-  #   spatial_points <- spatial_data[, c(longitude_column, longitude_column)]
-  #   names(spatial_points) <- c("LONG", "LAT")
-  #   spatial_points <- sp::SpatialPoints(
-  #     coords = spatial_points,
-  #     proj4string = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-  #   )
-  # }
-
-
-
-  # TODO - should we enforce these?
-  max_n_mesh <- c(48000, 16000)
-  max_n_strict_mesh <- c(128000, 128000)
-
-  create_mesh <- function(location_data, max_edge, cutoff, offset, crs) {
-    shiny::withProgress(message = "Creating mesh...", value = 0, {
-      fmesher::fm_mesh_2d(
-        loc = location_data,
-        max.edge = max_edge,
-        cutoff = cutoff,
-        offset = offset,
-        # crs = crs,
-        # max.n = c(48000, 16000),
-        # max.n.strict = c(128000, 128000)
-      )
-    })
-  }
-
   if (is.null(crs)) {
     crs <- sf::st_crs(spatial_data)
-    if (is.null(crs)) {
+    if (is.na(crs$input)) {
       stop("Unable to read CRS from data, please pass proj4string CRS to crs argument.")
+    }
+  } else {
+    # We try and create a CRS to make sure a valid CRS has been passed
+    if (is.character(crs)) {
+      crs <- sp::CRS(crs)
     }
   }
 
@@ -88,16 +52,31 @@ meshbuilder_shiny <- function(
     stop("Cannot read latitude and longitude data from spatial data.")
   }
 
-  # Extract the extent from the data
-  # extent <- raster::extent(spatial_data)
-  # # x is longitude, y is latitude
-  # long_min <- extent@xmin
-  # long_max <- extent@xmax
-  # lat_min <- extent@ymin
-  # lat_max <- extent@ymax
+  # Make sure we have our own internal correctly formatted version of the data
+  coords_only <- spatial_data[, c(longitude_column, latitude_column)]
+  names(coords_only) <- c("LONG", "LAT")
 
-  # Let's extract the data we want to create the mesh
-  location_data <- spatial_data[, c(longitude_column, latitude_column)]
+  plot_polygons <- FALSE
+  plot_points <- FALSE
+  # We may not use this
+  spatial_points <- NULL
+  # Do some checks to see what kind of data we have
+  # If we have a SpatialPolygonsDataFrame, we can plot the polygons
+  # otherwise if we just have SpatialPoints we can plot the points
+  # otherwise we don't plot anything
+  if (class(spatial_data) == "SpatialPolygonsDataFrame") {
+    plot_poly <- TRUE
+    plot_points <- FALSE
+  } else {
+    plot_poly <- FALSE
+    plot_points <- TRUE
+
+    spatial_points <- sp::SpatialPointsDataFrame(
+      coords = coords_only,
+      data = spatial_data,
+      proj4string = crs
+    )
+  }
 
   # loc: the spatial locations of data points
   # max.edge: it determines the maximum permitted length for a triangle (lower values for max.edge result in higher mesh resolution). This parameter can take either a scalar value, which controls the triangle edge lengths in the inner domain,
@@ -139,7 +118,6 @@ meshbuilder_shiny <- function(
     )
   )
 
-
   # Define server logic required to draw a histogram
   server <- function(input, output, session) {
     shiny::observeEvent(input$reset_mesh, {
@@ -149,13 +127,15 @@ meshbuilder_shiny <- function(
     })
 
     mesh <- shiny::eventReactive(input$plot_mesh, ignoreNULL = FALSE, {
-      create_mesh(
-        location_data,
-        input$max_edge,
-        input$cutoff,
-        input$offset,
-        crs
-      )
+      shiny::withProgress(message = "Creating mesh...", value = 0, {
+        fmesher::fm_mesh_2d(
+          loc = coords_only,
+          max.edge = input$max_edge,
+          cutoff = input$cutoff,
+          offset = input$offset,
+          crs = crs,
+        )
+      })
     })
 
     mesh_spatial <- shiny::reactive(
@@ -170,12 +150,11 @@ meshbuilder_shiny <- function(
       m <- leaflet::leaflet()
       m <- leaflet::addTiles(m, group = "OSM")
       m <- leaflet::addPolygons(m, data = mesh_spatial(), weight = 0.5, fillOpacity = 0.2, fillColor = "#5252ca", group = "Mesh")
-      if (plot_poly) {
-        m <- leaflet::addPolygons(m, data = spatial_data, lat = latitude_column, lng = longitude_column, fillColor = "#d66363", color = "green", weight = 1, group = "Spatial")
+      if (plot_polygons) {
+        m <- leaflet::addPolygons(m, data = spatial_data, fillColor = "#d66363", color = "green", weight = 1, group = "Spatial")
+      } else if (plot_points) {
+        m <- leaflet::addCircles(m, data = spatial_points, group = "Spatial", fillColor = "#b9220b")
       }
-      # else if (plot_points) {
-      #   m <- leaflet::addCircles(m, data = spatial_data_points)
-      # }
 
       m <- leaflet::addLayersControl(m,
         position = "topright",
