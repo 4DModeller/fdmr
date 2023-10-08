090ceab1aa2daafbb10802dbe44769261a5fd9e3
Gareth Jones <oxidising@gmail.com>
2023-10-08 11:52:52 +0100

    WIP: Looking at plotting mesh on map

--- R/plot_mesh.R
+++ R/plot_mesh.R
@@ -5,9 +5,36 @@
 #' @param point_colour Colour for points
 #' @param cex Point size magnifier
 #'
-#' @return NULL
+#' @return terra::plot
 #' @export
 plot_mesh <- function(mesh, point_data, point_colour = "blue", cex = 0.1) {
   terra::plot(mesh, main = "")
   terra::points(point_data, col = point_colour, cex = cex)
+}
+
+
+#' Plot a mesh on an interactive Leaflet map
+#'
+#' @param mesh Mesh data
+#'
+#' @return leaflet::leaflet
+#' @export
+plot_mesh_interactive <- function(mesh) {
+  spatial_mesh <- fdmr::mesh_to_spatial(mesh)
+
+  read_crs <- sp::proj4string(mesh)
+
+  if (!("+proj=longlat +datum=WGS84" %in% read_crs)) {
+    stop("Mesh must be in latlong WGS84 projection")
+  }
+
+  # Plotting on leaflet without just using WGS84 is tricky
+  crs <- leaflet::leafletCRS(
+    code = "EPSG:32632",
+    proj4def = "+proj=utm +zone=32"
+  )
+
+  leaflet::leaflet(options = leaflet::leafletOptions(crs = crs)) %>%
+    leaflet::addTiles(group = "OSM") %>%
+    leaflet::addPolygons(data = spatial_mesh, weight = 0.5, fillOpacity = 0.2, fillColor = "#5252ca", group = "Mesh")
 }
