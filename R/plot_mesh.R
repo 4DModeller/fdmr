8bc59ad89380b745da00e6a30cfcafd87a7d7878
Gareth Jones <oxidising@gmail.com>
2023-10-10 14:37:41 +0100

    WIP: Adding transform to mesh plotter

--- R/plot_mesh.R
+++ R/plot_mesh.R
@@ -1,16 +1,16 @@
-#' Plot a mesh
-#'
-#' @param mesh Mesh data
-#' @param point_data Points data
-#' @param point_colour Colour for points
-#' @param cex Point size magnifier
-#'
-#' @return terra::plot
-#' @export
-plot_mesh <- function(mesh, point_data, point_colour = "blue", cex = 0.1) {
-  terra::plot(mesh, main = "")
-  terra::points(point_data, col = point_colour, cex = cex)
-}
+# #' Plot a mesh
+# #'
+# #' @param mesh Mesh data
+# #' @param point_data Points data
+# #' @param point_colour Colour for points
+# #' @param cex Point size magnifier
+# #'
+# #' @return terra::plot
+# #' @export
+# plot_mesh <- function(mesh, point_data, point_colour = "blue", cex = 0.1) {
+#   terra::plot(mesh, main = "")
+#   terra::points(point_data, col = point_colour, cex = cex)
+# }
 
 
 #' Plot a mesh on an interactive Leaflet map
@@ -19,22 +19,23 @@ plot_mesh <- function(mesh, point_data, point_colour = "blue", cex = 0.1) {
 #'
 #' @return leaflet::leaflet
 #' @export
-plot_mesh_interactive <- function(mesh) {
-  spatial_mesh <- fdmr::mesh_to_spatial(mesh)
+plot_mesh <- function(mesh) {
+  expected_crs <- "+proj=longlat +datum=WGS84"
+  crs_string <- fmesher::fm_proj4string(mesh)
 
-  read_crs <- sp::proj4string(mesh)
-
-  if (!("+proj=longlat +datum=WGS84" %in% read_crs)) {
-    stop("Mesh must be in latlong WGS84 projection")
+  if (is.na(crs_string)) {
+    warning("Cannot read CRS from mesh, assuming WGS84")
+  } else if (!(expected_crs %in% crs_string)) {
+    warning("Transforming mesh from ", crs_string, " to ", expected_crs)
+    mesh <- fmesher::fm_transform(
+      x = mesh,
+      crs = sp::CRS(expected_crs)
+    )
   }
 
-  # Plotting on leaflet without just using WGS84 is tricky
-  crs <- leaflet::leafletCRS(
-    code = "EPSG:32632",
-    proj4def = "+proj=utm +zone=32"
-  )
+  spatial_mesh <- fdmr::mesh_to_spatial(mesh = mesh, crs = expected_crs)
 
-  leaflet::leaflet(options = leaflet::leafletOptions(crs = crs)) %>%
+  leaflet::leaflet() %>%
     leaflet::addTiles(group = "OSM") %>%
     leaflet::addPolygons(data = spatial_mesh, weight = 0.5, fillOpacity = 0.2, fillColor = "#5252ca", group = "Mesh")
 }
