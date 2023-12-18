cdaa6cd6d0ea598e8112fd285fd0ca98dd06e9cc
Gareth Jones <oxidising@gmail.com>
2023-12-18 16:26:44 +0000

    WIP: Updating meshbuilder with custom inputs

--- R/shiny_meshbuilder.R
+++ R/shiny_meshbuilder.R
@@ -1,13 +1,13 @@
 #' Mesh building shiny app
 #'
-#' @param spatial_data a data.frame or tibble containing spatial data
-#' @param data Observations data, for use with the check_mesh functionality
+#' @param spatial_data Spatial data
+#' @param obs_data Measurement data
 #' @param crs CRS as a proj4string
 #' @param offset Specifies the size of the inner and outer extensions around data locations, passed to fmesher::fm_mesh_2d_inla
 #' @param max_edge The largest allowed triangle edge length. One or two values, passed to fmesher::fm_mesh_2d_inla
 #' @param cutoff The minimum allowed distance between points, passed to fmesher::fm_mesh_2d_inla
-#' @param y_coord Name of the y coordinate (for example latitude) column in the spatial data
-#' @param x_coord Name of the  x coordinate (for example longitude) column in the spatial data
+#' @param y_coord Name of the latitude column in the spatial data
+#' @param x_coord Name of the longitude column in the spatial data
 #'
 #' @importFrom magrittr %>%
 #'
@@ -17,8 +17,8 @@ meshbuilder_shiny <- function(
     spatial_data,
     obs_data = NULL,
     crs = NULL,
-    max_edge = NULL,
     offset = NULL,
+    max_edge = NULL,
     cutoff = NULL,
     plot_poly = FALSE,
     y_coord = "LAT",
@@ -55,13 +55,23 @@ meshbuilder_shiny <- function(
     stop("Cannot read latitude and longitude data from spatial data. Please ensure given names are correct.")
   }
 
-  default_max_edge <- c(0.01, 0.3)
-  default_offset <- c(0.02, 0.7)
+  default_max_edge_min <- 0.01
+  default_max_edge_max <- 0.3
+  default_offset_min <- 0.02
+  default_offset_max <- 0.2
   default_cutoff <- 0.02
   # TODO - these defaults need changing?
-  if (is.null(max_edge)) max_edge <- default_max_edge
-  if (is.null(offset)) offset <- default_offset
-  if (is.null(cutoff)) cutoff <- default_cutoff
+  if (!is.null(max_edge)) {
+    default_max_edge_min <- max_edge[1]
+    default_max_edge_max <- max_edge[2]
+  }
+
+  if (!is.null(offset)) {
+    default_offset_min <- offset[1]
+    default_offset_max <- offset[2]
+  }
+
+  if (!is.null(cutoff)) default_cutoff <- cutoff
 
   # Make sure we have our own internal correctly formatted version of the data
   coords_only <- spatial_data[, c(x_coord, y_coord)]
@@ -77,22 +87,39 @@ meshbuilder_shiny <- function(
     shiny::sidebarLayout(
       shiny::sidebarPanel(
         shiny::checkboxInput(inputId = "enable_inputs", label = "Enable customisation", value = enable_inputs),
-        shiny::numericInput(
-          inputId = "max_edge",
-          label = "Max edge:",
-          value = 0.1
+        shiny::h4("Max edge"),
+        shiny::fluidRow(
+          shiny::numericInput(
+            inputId = "max_edge_min",
+            label = "Min:",
+            value = default_max_edge_min
+          ),
+          shiny::numericInput(
+            inputId = "max_edge_max",
+            label = "Max:",
+            value = default_max_edge_max
+          )
         ),
         shiny::p("Max permitted edge length for a triangle"),
-        shiny::numericInput(
-          inputId = "offset",
-          label = "Offset:",
-          value = 0.1
+        shiny::h4("Offset"),
+        shiny::fluidRow(
+          shiny::numericInput(
+            inputId = "offset_min",
+            label = "Min:",
+            value = default_offset_min
+          ),
+          shiny::numericInput(
+            inputId = "offset_max",
+            label = "Max:",
+            value = default_offset_max
+          )
         ),
         shiny::p("Specifies the size of the inner and outer extensions around data locations."),
+        shiny::h4("Cutoff"),
         shiny::numericInput(
           inputId = "cutoff",
-          label = "Cutoff:",
-          value = 0.1
+          label = NULL,
+          value = default_cutoff
         ),
         shiny::p("Minimum allowed distance between data points."),
         shiny::actionButton("plot_mesh", label = "Plot mesh"),
@@ -130,20 +157,26 @@ meshbuilder_shiny <- function(
   server <- function(input, output, session) {
     shiny::observeEvent(input$enable_inputs, {
       if (input$enable_inputs) {
-        shinyjs::enable("max_edge")
-        shinyjs::enable("offset")
+        shinyjs::enable("max_edge_min")
+        shinyjs::enable("max_edge_max")
+        shinyjs::enable("offset_min")
+        shinyjs::enable("offset_max")
         shinyjs::enable("cutoff")
       } else {
-        shinyjs::disable("max_edge")
-        shinyjs::disable("offset")
+        shinyjs::disable("max_edge_min")
+        shinyjs::disable("max_edge_max")
+        shinyjs::disable("offset_min")
+        shinyjs::disable("offset_max")
         shinyjs::disable("cutoff")
       }
     })
 
     shiny::observeEvent(input$reset_mesh, {
-      shiny::updateSliderInput(session, inputId = "max_edge", value = default_max_edge)
-      shiny::updateSliderInput(session, inputId = "offset", value = default_offset)
-      shiny::updateSliderInput(session, inputId = "cutoff", value = default_cutoff)
+      shiny::updateNumericInput(session, inputId = "max_edge_min", value = default_max_edge_min)
+      shiny::updateNumericInput(session, inputId = "max_edge_max", value = default_max_edge_max)
+      shiny::updateNumericInput(session, inputId = "offset_min", value = default_offset_min)
+      shiny::updateNumericInput(session, inputId = "offset_max", value = default_offset_max)
+      shiny::updateNumericInput(session, inputId = "cutoff", value = default_cutoff)
     })
 
     output$mesh_crs <- shiny::renderText({
@@ -152,8 +185,8 @@ meshbuilder_shiny <- function(
 
     mesh <- shiny::eventReactive(input$plot_mesh, ignoreNULL = FALSE, {
       if (input$enable_inputs) {
-        max_edge <- input$max_edge
-        offset <- input$offset
+        max_edge <- c(input$max_edge_min, input$max_edge_max)
+        offset <- c(input$offset_min, input$offset_max)
         cutoff <- input$cutoff
       } else {
         max_edge <- NULL
